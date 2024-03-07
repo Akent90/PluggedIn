5aa2665c0c6b24e75aa95761aaece43af6a5d352
akent <alexanderkent099@gmail.com>
2024-03-07 12:57:03 -0500

    Adding playlist model

---
+++ models/Playlist.js
@@ -0,0 +1,48 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Playlist extends Model {}
+
+Playlist.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false, 
+        primaryKey: true, 
+        autoIncrement: true 
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    mood: {
+        type: DataTypes.STRING, 
+        allowNull: false
+    },
+    description: {
+        type: DataTypes.TEXT,
+        allowNull: true
+    },
+    soundcloudUrl: {
+        type: DataTypes.STRING, 
+        allowNull: false, 
+        validate: {
+            isUrl: true
+        }
+    },
+    userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'user',
+            key: 'id',
+        }
+    }
+}, {
+    sequelize, 
+    timestamps: true, 
+    freezeTableName: true, 
+    underscored: true, 
+    modelName: 'playlist'
+});
+
+module.exports = Playlist;
