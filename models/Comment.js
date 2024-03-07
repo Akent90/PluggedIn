2a1fec371cbb84ad84e5e45e3a6f43ccd8dc43b3
akent <alexanderkent099@gmail.com>
2024-03-07 13:02:09 -0500

    Adding comment model

---
+++ models/Comment.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false, 
+        primaryKey: true, 
+        autoIncrement: true
+    },
+    content: {
+        type: DataTypes.TEXT, 
+        allowNull: false
+    },
+    userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false, 
+        references: {
+            model: 'user', 
+            key: 'id',
+        }
+    },
+    playlistId: {
+        type: DataTypes.INTEGER,
+        allowNull: false, 
+        references: {
+            model: 'playlist',
+            key: 'id',
+        }
+    }
+}, {
+    sequelize, 
+    timestamps: true, 
+    freezeTableName: true, 
+    underscored: true, 
+    modelName: 'comment'
+});
+
+module.exports = Comment;
